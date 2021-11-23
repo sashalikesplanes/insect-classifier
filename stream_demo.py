6b82ecb574c354b536bd1ed58d5f1d55f97094e4
sashalikesplanes <a.kiselev@student.tudelft.nl>
2021-11-23 11:49:43 +0100

    Update stream_demo.py

--- stream_demo.py
+++ stream_demo.py
@@ -2,15 +2,19 @@ import streamlit as st
 from fastbook import load_learner, PILImage
 
 
+# load the learner and add the function needed for the learner 
 def return_list(x): return [x] #need to add this custom function from learner
 inferer = load_learner('multimod.pkl')
 
+# setup the title and description of web app
 st.title("Insect Inferer")
 st.write("A simple web app where a user may upload the picture of an insect found in their house and it is classified into one of the top 7 common insects (spiders, mosquitos, fruit flies, bed bugs, cockroaches, moths, silverfish) or informs the user that it does not know the insect.")
 
 uploaded_file = st.file_uploader("Choose an image with an insect", type=["jpg", "jpeg", "heic"])
-output = st.empty()
-if uploaded_file is not None:
+
+output = st.empty() # make an empty object to be used to write the text for the prediction
+
+if uploaded_file is not None: # once an image is uploaded run inference
     image = PILImage.create(uploaded_file)
     output.text("Classifying...")
     st.image(image, caption='Your image', width=512, use_column_width=False)
@@ -19,8 +23,6 @@ if uploaded_file is not None:
     if probs.max().item() > 0.7:
         output.text(f"I am guessing this is a {label[0]} with a probability of: {probs.max().item() * 100:.04f}%")
     else:
-        output.text("Excuse me I am not sure what this is")
-    
-    #label = predict(uploaded_file)
-    #st.write('%s (%.2f%%)' % (label[1], label[2]*100))
+        output.text("Excuse me, I am not sure what this is")
+
 
