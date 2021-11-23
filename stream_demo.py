9ed545db8d114aa4c0ed4c616dcc5d05583d8102
sashalikesplanes <a.kiselev@student.tudelft.nl>
2021-11-23 01:40:39 +0100

    Update stream_demo.py

--- stream_demo.py
+++ stream_demo.py
@@ -11,14 +11,14 @@ st.write("A simple web app where a user may upload the picture of an insect foun
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "heic"])
 if uploaded_file is not None:
     image = PILImage.create(uploaded_file)
+    out = st.write("Classifying...")
     st.image(image, caption='Uploaded Image.', width=256, use_column_width=False)
-    st.write("Classifying...")
     st.write("")
     label, _, probs = inferer.predict(image)
     if probs.max().item() > 0.7:
-        st.write(f"I am guessing this is a {label[0]} with a probability of: {probs.max().item() * 100:.04f}%")
+        out = st.write(f"I am guessing this is a {label[0]} with a probability of: {probs.max().item() * 100:.04f}%")
     else:
-        st.write("Excuse me I am not sure what this is")
+        out = st.write("Excuse me I am not sure what this is")
     
     #label = predict(uploaded_file)
     #st.write('%s (%.2f%%)' % (label[1], label[2]*100))
