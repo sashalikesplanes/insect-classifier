a639b66133122967f8eb4ba245806dde35ecfa95
sashalikesplanes <a.kiselev@student.tudelft.nl>
2021-11-23 01:30:05 +0100

    Update stream_demo.py

--- stream_demo.py
+++ stream_demo.py
@@ -6,7 +6,7 @@ def return_list(x): return [x] #need to add this custom function from learner
 inferer = load_learner('multimod.pkl')
 
 st.title("Insect Inferer")
-st.write("a simple web app where a user may upload the picture of an insect found in their house and it is classified into one of the top 7 common insects (spiders, mosquitos, fruit flies, bed bugs, cockroaches, moths, silverfish) or informs the user that it does not know the insect.")
+st.write("A simple web app where a user may upload the picture of an insect found in their house and it is classified into one of the top 7 common insects (spiders, mosquitos, fruit flies, bed bugs, cockroaches, moths, silverfish) or informs the user that it does not know the insect.")
 
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "heic"])
 if uploaded_file is not None:
