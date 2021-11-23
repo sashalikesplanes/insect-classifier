68dce2f53a4045af6442fcd2b2d1a43e01950849
sashalikesplanes <a.kiselev@student.tudelft.nl>
2021-11-23 01:47:52 +0100

    Update stream_demo.py

--- stream_demo.py
+++ stream_demo.py
@@ -9,16 +9,17 @@ st.title("Insect Inferer")
 st.write("A simple web app where a user may upload the picture of an insect found in their house and it is classified into one of the top 7 common insects (spiders, mosquitos, fruit flies, bed bugs, cockroaches, moths, silverfish) or informs the user that it does not know the insect.")
 
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "heic"])
+output = st.empty()
 if uploaded_file is not None:
     image = PILImage.create(uploaded_file)
-    out = st.write("Classifying...")
+    output.text("Classifying...")
     st.image(image, caption='Uploaded Image.', width=256, use_column_width=False)
     st.write("")
     label, _, probs = inferer.predict(image)
     if probs.max().item() > 0.7:
-        out = "I am guessing this is a {label[0]} with a probability of: {probs.max().item() * 100:.04f}%"
+        output.text("I am guessing this is a {label[0]} with a probability of: {probs.max().item() * 100:.04f}%")
     else:
-        out = "Excuse me I am not sure what this is"
+        output.text("Excuse me I am not sure what this is")
     
     #label = predict(uploaded_file)
     #st.write('%s (%.2f%%)' % (label[1], label[2]*100))
