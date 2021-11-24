effc2ac40beb73254becb55607c632bc184e9f30
sashalikesplanes <a.kiselev@student.tudelft.nl>
2021-11-24 19:58:07 +0100

    Update stream_demo.py

--- stream_demo.py
+++ stream_demo.py
@@ -6,10 +6,10 @@ from fastbook import load_learner, PILImage
 def return_list(x): return [x] #need to add this custom function from learner
 inferer = load_learner('multimod.pkl')
 # setup the title and description of web app
-st.title("Insect Inferer")
+st.title("Insect Classifier")
 st.write("A simple web app where a user may upload the picture of an insect found in their house and it is classified into one of the top 7 common insects (spiders, mosquitos, fruit flies, bed bugs, cockroaches, moths, silverfish) or informs the user that it does not know the insect.")
 
-uploaded_file = st.file_uploader("Choose an image with an insect", type=["jpg", "jpeg", "heic"])
+uploaded_file = st.file_uploader("Choose an image with an insect", type=["jpg", "jpeg", "heic", "png"])
 
 output = st.empty() # make an empty object to be used to write the text for the prediction
 
